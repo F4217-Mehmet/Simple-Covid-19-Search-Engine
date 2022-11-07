9ba9bce6df2ecca31ef146640b3d82b652cdbf6e
Mehmet Kabasakal <sehriistanbul@gmail.com>
2022-11-08 01:31:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
-import { useState } from "react";
+
 
 function App() {
 
-  const [form, setForm]= useState({name:"", surname:"", age:""});
-  const {name, surname, age} = form;
-
-  const onChangeInput = (e)=> {setForm({...form, [e.target.name]: e.target.value})}
 
   return (
-    <>
-      Name: <br />
-      <input name="name" value={name} onChange={onChangeInput} /> <br />
-      Surname: <br />
-      <input name="surname" value={surname} onChange={onChangeInput} /> <br /> 
-      Age: <br />
-      <input name="age" value= {age} onChange={onChangeInput} /> <br />  <br />
-      <div>{name} {surname} is {age} years old.</div>
-    </>
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-white text-center display-4 mt-2">
+              TÜRKİYE COVID-19 ARAMA MOTORU
+            </h2>
+            <input
+              type="text"
+              placeholder="GG/AA/YY"
+              className="form-control"
+            />
+            <table class="table table-dark table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Test Sayısı</th>
+                  <th scope="col">Hasta Sayısı</th>
+                  <th scope="col">Vefat Sayısı</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>300.000</td>
+                  <td>45.000</td>
+                  <td>250</td>
+                </tr>
+
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
