d1906b1da3be67311bfb332fbdc11b1c3866d2c9
Mehmet Kabasakal <sehriistanbul@gmail.com>
2022-11-07 20:00:50 +0300

    middle of the study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [form, setForm]= useState({name:"", surname:"", age:""});
+  const {name, surname, age} = form;
+
+  const onChangeInput = (e)=> {setForm({...form, [e.target.name]: e.target.value})}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Name: <br />
+      <input name="name" value={name} onChange={onChangeInput} /> <br />
+      Surname: <br />
+      <input name="surname" value={surname} onChange={onChangeInput} /> <br /> 
+      Age: <br />
+      <input name="age" value= {age} onChange={onChangeInput} /> <br />  <br />
+      <div>{name} {surname} is {age} years old.</div>
+    </>
   );
 }
 
